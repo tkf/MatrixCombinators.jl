56e8881781c658b4daeb47a8444afe7d739c4fc0
Takafumi Arakaki <aka.tkf@gmail.com>
2018-06-02 18:39:20 -0700

    Document gem[mv]! wrappers

--- src/gem_wrappers.jl
+++ src/gem_wrappers.jl
@@ -2,7 +2,22 @@
     const BLAS = LinearAlgebra.BLAS
 end
 
+"""
+    _gemm!(tA, tB, α, A, B, β, C)
 
+`BLAS.gemm!` wrapper; i.e., it does `C = α * Â * B̂ + β * C` where `Â`
+(`B̂`) is `A` (`B`) itself or its transpose or adjoint depending on the
+value of `tA` (`tB`).
+
+See also:
+
+- [LinearAlgebra.BLAS.gemm!](https://docs.julialang.org/en/latest/stdlib/LinearAlgebra/#LinearAlgebra.BLAS.gemm!)
+
+`BLAS.gemm!` is called if `A` is one of:
+
+- [BlockBandedMatrices.BlockBandedBlock](https://github.com/JuliaMatrices/BlockBandedMatrices.jl/blob/v0.0.1/src/linalg.jl#L60)
+- [Array](https://github.com/JuliaLang/julia/blob/v0.7.0-alpha/stdlib/LinearAlgebra/src/blas.jl#L1084)
+"""
 function _gemm!(tA, tB,
                 alpha::T,
                 A::AbstractVecOrMat{T},
@@ -26,9 +41,29 @@ function _gemm!(tA, tB,
     BLAS.gemm!(tA, tB, alpha, A, B, beta, C)
 end
 
+
+"""
+    _gemv!(tA, α, A, X, β, Y)
+
+`BLAS.gemv!` wrapper; i.e., it does `y = α * Â * x + β * y` where `Â`
+is `A` itself or its transpose or adjoint depending on the value of
+`tA`.
+
+See also:
+
+- [LinearAlgebra.BLAS.gemv!](https://docs.julialang.org/en/latest/stdlib/LinearAlgebra/#LinearAlgebra.BLAS.gemv!)
+"""
 _gemv!(tA, alpha, A, X, beta, Y) = BLAS.gemv!(tA, alpha, A, X, beta, Y)
 
 
+"""
+When `A` isa `SparseMatrixCSC`, `_gemm!` calls `SparseMatrixCSC`'s
+GEMM-like interface.
+
+See:
+- [base/sparse/linalg.jl (v0.6.3)](https://github.com/JuliaLang/julia/blob/v0.6.3/base/sparse/linalg.jl#L46)
+- [stdlib/SparseArrays/src/linalg.jl (v0.7.0-alpha)](https://github.com/JuliaLang/julia/blob/v0.7.0-alpha/stdlib/SparseArrays/src/linalg.jl#L32)
+"""
 function _gemm!(tA, tB,
                 alpha::Number,
                 A::SparseMatrixCSC,
@@ -46,6 +81,14 @@ function _gemm!(tA, tB,
     end
 end
 
+"""
+When `A` isa `SparseMatrixCSC`, `_gemv!` calls `SparseMatrixCSC`'s
+GEMM-like interface (via `_gemm!`).
+
+See:
+- [base/sparse/sparsevector.jl (v0.6.3)](https://github.com/JuliaLang/julia/blob/v0.6.3/base/sparse/sparsevector.jl#L1532)
+- [stdlib/SparseArrays/src/sparsevector.jl (v0.7.0-alpha)](https://github.com/JuliaLang/julia/blob/v0.7.0-alpha/stdlib/SparseArrays/src/sparsevector.jl#L1477)
+"""
 function _gemv!(tA,
                 alpha::Number,
                 A::SparseMatrixCSC,
